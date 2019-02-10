a4119f7da78c168ac4310b04b541d43df6be52a6
MathiasReker <mathias@reker.dk>
2019-02-10 13:50:58 +0100

    Beautify JS files

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,18 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
-    "indent": ["error", 4],
+    indent: ['error', 4],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: 'babel-eslint',
+  },
+};
